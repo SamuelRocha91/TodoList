966b62874a4314851f658d6da10c6cb083005794
Samuel Rocha <samuel_sr@hotmail.com.br>
2022-10-05 20:57:47 -0300

    solucionando 5o e 6os desafios

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// VARIÁVEL GLOBAL
+// VARIÁVEIS GLOBAIS
 const corpo = document.body;
+
 // PRIMEIRA QUESTÃO
 function createHeader() {
   const header = document.createElement('header');
@@ -15,11 +16,15 @@ function createParagraph () {
   corpo.appendChild(paragraph);
 }
 createParagraph();
+const form = document.createElement('form');
+form.id = 'addFrases';
+corpo.appendChild(form)
 // TERCEIRA QUESTÃO
 function createInput() {
   const input = document.createElement('input');
+  input.type = 'text';
   input.id = 'texto-tarefa';
-  corpo.appendChild(input);
+  form.appendChild(input);
 }
 createInput();
 // QUARTA QUESTÃO
@@ -29,3 +34,34 @@ function createOrderedList() {
   corpo.appendChild(ol)
 }
 createOrderedList();
+// QUINTA QUESTÃO
+function createButton() {
+  const button = document.createElement('button');
+  button.id = 'criar-tarefa';
+  button.type = 'submit';
+  button.innerText = 'Criar Tarefa';
+  form.appendChild(button);
+}
+createButton();
+const input = document.getElementById('texto-tarefa');
+console.log(input.value);
+const botao = document.getElementById('criar-tarefa');
+let arrayDeFrases = [];
+botao.addEventListener('click', gravaValores);
+function gravaValores(evento) {
+  evento.preventDefault();
+  let texto = input.value;
+  let guardado = JSON.parse(localStorage.getItem('frases')) || [];
+  guardado.push(texto);
+  localStorage.setItem('frases', JSON.stringify(guardado));
+  input.value = "";
+  escreveNoDOm();
+}
+const ol = document.getElementById('lista-tarefas');
+function escreveNoDOm() {
+  const li = document.createElement('li');
+  let recebeFrases = JSON.parse(localStorage.getItem('frases'));
+  let tamanho = recebeFrases.length - 1;
+  li.innerText = recebeFrases[tamanho];
+  ol.appendChild(li);
+}
